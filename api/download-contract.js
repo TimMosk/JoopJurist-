8136a28f0ba23492d4d3c63026f6fac75783998d
TimMosk <timmosk@hotmail.com>
2025-09-05 09:42:13 +0200

    Update download-contract.js

--- api/download-contract.js
+++ api/download-contract.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Concept is required" });
     }
 
-    // Create RTF format - this opens properly in Word
+    // RTF content met betere formatting
     const rtfContent = `{\\rtf1\\ansi\\deff0
 {\\fonttbl{\\f0\\fswiss\\fcharset0 Arial;}}
 {\\colortbl;\\red37\\green99\\blue235;}
@@ -18,8 +18,9 @@ ${concept.replace(/\*\*(.*?)\*\*/g, '\\b $1\\b0').replace(/\n/g, '\\par ')}\\par
 \\i Dit document is gegenereerd door JoopJurist.nl\\i0
 }`;
 
-    res.setHeader('Content-Type', 'application/rtf');
-    res.setHeader('Content-Disposition', `attachment; filename="${filename}.rtf"`);
+    // Gebruik .doc extensie - dit opent automatisch in Word
+    res.setHeader('Content-Type', 'application/msword');
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}.doc"`);
     res.send(rtfContent);
     
   } catch (error) {
